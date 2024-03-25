db65d38b155c89f5fd1df25bbe82f6993eb24934
Vivek Selvaraj <vivek.selvaraj@me.com>
2024-03-24 21:55:44 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
-let h1 = document.querySelector("h1");
-h1.innerHTML = "Vivek's Custom Heading!";
+let instructionButton = document.getElementById("instruction_button");
+instructionButton.addEventListener("click", function() {
+    let instructions = document.querySelectorAll(".question");
+    instructions.forEach((element) => {
+        element.classList.toggle("hidden");
+    });
+    if (this.textContent.includes("Show")) {
+        this.textContent = "Hide Instructions";
+    } else {
+        this.textContent = "Show Instructions";
+    }
+});
+
+
+
+let filePicker = document.getElementById("profile_pic");
+filePicker.addEventListener("change", () => {
+    if (filePicker.files.length == 1) {
+      console.log("File selected: ", filePicker.files[0]);
+      let size = filePicker.files[0].size;
+      if (size > 100 * 1024) {
+        alert("Size is greater than 100KB");
+      }
+    }
+  });
+
+
+  
